ddae36247f699da5a2d64fbbfc90488749db5398
Clark Rodriguez Millan <740394@student.dusd.net>
2024-03-18 15:26:53 -0700

    2.3 lesson 5
    made everything up until changing the camera on lesson 5 of 2.3

--- Unit 2 Basic Clark/Assets/Scripts/DestroyOutOfBounds.cs
+++ Unit 2 Basic Clark/Assets/Scripts/DestroyOutOfBounds.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class DestroyOutOfBounds : MonoBehaviour
 {
     private float topBound = 30;
+    private float lowerBound = -10;
     // Start is called before the first frame update
     void Start()
     {
